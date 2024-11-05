ed3b84a9b4f0bf6e11ea40edf79d4a0105bd807d
Denis Tuzhik <dev@dentuzhik.com>
2024-11-05 18:05:16 +0200

    feat: Migrate tsconfig-loader to use native typescript api

--- src/tsconfig-loader.ts
+++ src/tsconfig-loader.ts
@@ -1,9 +1,10 @@
 import * as path from "path";
-import * as fs from "fs";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import JSON5 = require("json5");
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import StripBom = require("strip-bom");
+import {
+  sys as tsSys,
+  findConfigFile,
+  readConfigFile,
+  parseJsonConfigFileContent,
+} from "typescript";
 
 /**
  * Typing for the parts of tsconfig that we care about
@@ -52,178 +53,40 @@ function loadSyncDefault(
   filename?: string,
   baseUrl?: string
 ): TsConfigLoaderResult {
-  // Tsconfig.loadSync uses path.resolve. This is why we can use an absolute path as filename
-
-  const configPath = resolveConfigPath(cwd, filename);
-
-  if (!configPath) {
-    return {
-      tsConfigPath: undefined,
-      baseUrl: undefined,
-      paths: undefined,
-    };
-  }
-  const config = loadTsconfig(configPath);
-
-  return {
-    tsConfigPath: configPath,
-    baseUrl:
-      baseUrl ||
-      (config && config.compilerOptions && config.compilerOptions.baseUrl),
-    paths: config && config.compilerOptions && config.compilerOptions.paths,
+  const earlyReturn = {
+    tsConfigPath: undefined,
+    baseUrl: undefined,
+    paths: undefined,
   };
-}
-
-function resolveConfigPath(cwd: string, filename?: string): string | undefined {
   if (filename) {
-    const absolutePath = fs.lstatSync(filename).isDirectory()
-      ? path.resolve(filename, "./tsconfig.json")
-      : path.resolve(cwd, filename);
-
-    return absolutePath;
+    cwd = filename;
   }
 
-  if (fs.statSync(cwd).isFile()) {
-    return path.resolve(cwd);
+  const tsConfigPath = findConfigFile(
+    path.dirname(cwd),
+    tsSys.fileExists,
+    path.basename(cwd)
+  );
+
+  if (!tsConfigPath) {
+    return earlyReturn;
   }
 
-  const configAbsolutePath = walkForTsConfig(cwd);
-  return configAbsolutePath ? path.resolve(configAbsolutePath) : undefined;
-}
-export function walkForTsConfig(
-  directory: string,
-  readdirSync: (path: string) => string[] = fs.readdirSync
-): string | undefined {
-  const files = readdirSync(directory);
-  const filesToCheck = ["tsconfig.json", "jsconfig.json"];
-  for (const fileToCheck of filesToCheck) {
-    if (files.indexOf(fileToCheck) !== -1) {
-      return path.join(directory, fileToCheck);
-    }
+  const tsconfigFileOrError = readConfigFile(tsConfigPath, tsSys.readFile);
+  if (tsconfigFileOrError.error) {
+    return earlyReturn;
   }
 
-  const parentDirectory = path.dirname(directory);
-
-  // If we reached the top
-  if (directory === parentDirectory) {
-    return undefined;
-  }
-
-  return walkForTsConfig(parentDirectory, readdirSync);
-}
-
-export function loadTsconfig(
-  configFilePath: string,
-  // eslint-disable-next-line no-shadow
-  existsSync: (path: string) => boolean = fs.existsSync,
-  readFileSync: (filename: string) => string = (filename: string) =>
-    fs.readFileSync(filename, "utf8")
-): Tsconfig | undefined {
-  if (!existsSync(configFilePath)) {
-    return undefined;
-  }
-
-  const configString = readFileSync(configFilePath);
-  const cleanedJson = StripBom(configString);
-  let config: Tsconfig;
-  try {
-    config = JSON5.parse(cleanedJson);
-  } catch (e) {
-    throw new Error(`${configFilePath} is malformed ${e.message}`);
-  }
-
-  let extendedConfig = config.extends;
-  if (extendedConfig) {
-    let base: Tsconfig;
-
-    if (Array.isArray(extendedConfig)) {
-      base = extendedConfig.reduce(
-        (currBase, extendedConfigElement) =>
-          mergeTsconfigs(
-            currBase,
-            loadTsconfigFromExtends(
-              configFilePath,
-              extendedConfigElement,
-              existsSync,
-              readFileSync
-            )
-          ),
-        {}
-      );
-    } else {
-      base = loadTsconfigFromExtends(
-        configFilePath,
-        extendedConfig,
-        existsSync,
-        readFileSync
-      );
-    }
-
-    return mergeTsconfigs(base, config);
-  }
-  return config;
-}
-
-/**
- * Intended to be called only from loadTsconfig.
- * Parameters don't have defaults because they should use the same as loadTsconfig.
- */
-function loadTsconfigFromExtends(
-  configFilePath: string,
-  extendedConfigValue: string,
-  // eslint-disable-next-line no-shadow
-  existsSync: (path: string) => boolean,
-  readFileSync: (filename: string) => string
-): Tsconfig {
-  if (
-    typeof extendedConfigValue === "string" &&
-    extendedConfigValue.indexOf(".json") === -1
-  ) {
-    extendedConfigValue += ".json";
-  }
-  const currentDir = path.dirname(configFilePath);
-  let extendedConfigPath = path.join(currentDir, extendedConfigValue);
-  if (
-    extendedConfigValue.indexOf("/") !== -1 &&
-    extendedConfigValue.indexOf(".") !== -1 &&
-    !existsSync(extendedConfigPath)
-  ) {
-    extendedConfigPath = path.join(
-      currentDir,
-      "node_modules",
-      extendedConfigValue
-    );
-  }
-
-  const config =
-    loadTsconfig(extendedConfigPath, existsSync, readFileSync) || {};
-
-  // baseUrl should be interpreted as relative to extendedConfigPath,
-  // but we need to update it so it is relative to the original tsconfig being loaded
-  if (config.compilerOptions?.baseUrl) {
-    const extendsDir = path.dirname(extendedConfigValue);
-    config.compilerOptions.baseUrl = path.join(
-      extendsDir,
-      config.compilerOptions.baseUrl
-    );
-  }
-
-  return config;
-}
-
-function mergeTsconfigs(
-  base: Tsconfig | undefined,
-  config: Tsconfig | undefined
-): Tsconfig {
-  base = base || {};
-  config = config || {};
+  const parsedConfig = parseJsonConfigFileContent(
+    tsconfigFileOrError.config,
+    tsSys,
+    path.dirname(tsConfigPath)
+  );
+  const compilerOptions = parsedConfig.options;
 
   return {
-    ...base,
-    ...config,
-    compilerOptions: {
-      ...base.compilerOptions,
-      ...config.compilerOptions,
-    },
+    tsConfigPath,
+    baseUrl: baseUrl || (compilerOptions && compilerOptions.baseUrl),
+    paths: compilerOptions && compilerOptions.paths,
   };
 }
